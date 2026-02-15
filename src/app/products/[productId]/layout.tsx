78d26ba04d0152e31e5423388487efb7499d7cb2
matheustakasaki <matheus8antunes@gmail.com>
2026-02-15 00:18:17 -0300

    Implementing Nested Layouts concept in productId folder.

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,10 @@
+export default function ProductDetailsLayout({
+  children,
+}: {    children: React.ReactNode}) {
+  return (
+    <div>
+        {children}
+        <h2>Featured products</h2>
+    </div>
+  )
+}
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,7 @@
-export default function ProductDetails(){
-    return <h1>Product Details</h1>
+
+// This is the page component for the dynamic route /products/[productId]. It receives the productId as a parameter and displays it on the page. It will be wrapped by the ProductDetailsLayout defined in layout.tsx, which adds a section for featured products below the product details.
+export default async function ProductDetails({ params }: { params: Promise<{ productId: string }> }) {
+    const { productId } = await params;
+
+    return <h1>Product Details for ID: {productId}</h1>
 }
