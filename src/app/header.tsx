6c2363fbfb62994fa4bbcaeae037351ed685d4af
matheustakasaki <matheus8antunes@gmail.com>
2026-02-14 23:58:50 -0300

    Layouts in practice - creating shareable components inside application

---
+++ src/app/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header   style={{
+            backgroundColor: '#333',
+            color: '#fff',
+            padding: '1rem',
+            textAlign: 'center',
+          }}>
+            Esse é o header
+        </header>
+    )
+}
