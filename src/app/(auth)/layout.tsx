3664c49719a989b144c190c361381f483f6dcf80
matheustakasaki <matheus8antunes@gmail.com>
2026-02-18 10:41:10 -0300

    Using Link and implementing active link in the application with usePathname hook. Implementing also tailwind classes into our components.

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import './styles.css';
+
+const navLinks = [
+    { href: '/login', name: 'Login' },
+    { href: '/register', name: 'Register' },
+    { href: '/forgot-password', name: 'Forgot Password' },
+];
+
+
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const pathname = usePathname();
+    console.log('Current Pathname:', pathname);
+    return (
+        <div>
+            {
+                navLinks.map((link) => {
+                    const isactive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== '/');
+                    return (
+                        <Link key={link.href} href={link.href} style={{ marginRight: '1rem', fontWeight: isactive ? 'bold' : 'normal' }}
+                        className={isactive ? 'font-bold mr-4' : 'text-blue-500 mr-4'}
+                        >
+                            {link.name}
+                        </Link>
+                    )
+                })}
+            {children}
+        </div>
+    )
+}
