39786a98275c1b23c1c11beeb8dd86a29be4713e
matheustakasaki <matheus8antunes@gmail.com>
2026-02-18 10:23:16 -0300

    Using Next.js link component, dynamic link and replace prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
+// Import Links from next/link
+import Link from 'next/link'
 
-export default function page() {
-    <h1>Welcome</h1>
+export default function Home() {
+
+    return (
+
+        <>
+            <h1>Welcome</h1>
+            <Link href="/about">Go to About Page</Link>
+            <Link href="/products">Go to Products Page</Link>
+        </>
+    )
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,22 @@
+import Link from 'next/link'
+
 export default function ProductList() {
 
+    const productId = 1;
     return (
         <>
             <h1>Product List</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <Link href="/">Home</Link>
+            <h2>
+                {/* Dynamic Link */}
+                <Link href={`/products/${productId}`}>Product 1</Link>
+            </h2>
+            <h2>
+                <Link href="/products/2">Product 2</Link>
+            </h2>
+            <h2>
+                <Link href="/products/3" replace>Product 3</Link>
+            </h2>
         </>
     )
 }
