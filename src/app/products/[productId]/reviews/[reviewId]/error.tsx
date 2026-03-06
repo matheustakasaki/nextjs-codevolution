7b59077b46a518d2e115a865ef99f6b5407c00f5
matheustakasaki <matheus8antunes@gmail.com>
2026-03-05 23:32:22 -0300

    Recovering from errors - useful when we catch a temporaty error or an instability problem in our application.

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,26 @@
 "use client";
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
 
-export default function ErrorBoundary (){
+
+export default function ErrorBoundary ({error, reset }: {
+    error: Error,
+    reset: () => void
+}){
+    const router = useRouter();
+
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        })
+    }
     return (
+
         <>
-            Error in loading the review details. Please try again later.
+        <p>{error.message}</p>
+        {/* try again button rerender the component */}
+        <button onClick={() => reload()}>Try Again</button>
         </>
     )
 }
